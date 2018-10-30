bc82e805c774193bbb2b3275d4ae477adfec7d48
amzad hossain <amzad.hossain@baruchmail.cuny.edu>
2018-10-30 14:30:12 -0400

    add serch functionalityt to the app

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,6 +1,40 @@
 import React, { Component } from 'react'
+import * as BooksAPI from './BooksAPI'
+import Book from './Book'
 
 class SearchPage extends  Component {
+  state = {
+    query: '',
+    searchBooks: []
+  }
+
+  updateQuery = (query) => {
+    this.setState({
+      query: query
+    })
+    this.updateSearchBooks(query);
+  }
+
+  updateSearchBooks = (query) => {
+    if (query) {
+      BooksAPI.search(query)
+     .then((searchBooks) => {
+        if (searchBooks.error) {
+          this.setState({
+            searchBooks: []
+          })
+        } else {
+          this.setState({
+            searchBooks
+          })
+        }
+      })
+    } else {
+      this.setState({
+        searchBooks: []
+      })
+    }
+  }
   render() {
     return (
       <div className="search-books">
@@ -15,12 +49,25 @@ class SearchPage extends  Component {
                 However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                 you don't find a specific author or title. Every search is limited by search terms.
               */}
-              <input type="text" placeholder="Search by title or author"/>
+              <input
+                type="text"
+                placeholder="Search by title or author"
+                value={this.state.query}
+                onChange={(event) => this.updateQuery(event.target.value)}
+                />
 
             </div>
           </div>
           <div className="search-books-results">
-            <ol className="books-grid"></ol>
+            <ol className="books-grid">
+              {
+                this.state.searchBooks.map(searchBook => (
+                  <li key={searchBook.id}>
+                    <Book book={searchBook} />
+                  </li>
+                ))
+              }
+            </ol>
           </div>
       </div>
     );
